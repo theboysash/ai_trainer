467db1e44abbc8a216393b7f46d497e4ebe02d43
theboysash <2733820@students.wits.ac.za>
2025-09-22 13:31:00 +0200

    n

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Pose Tracker MVP</h1>
+      <p>We’ll add MediaPipe + Firebase next.</p>
+    </div>
+  );
+}
