f2acd2b1f1ffe509d57470536d8cc30ca4b5506d
theboysash <2733820@students.wits.ac.za>
2025-09-27 20:43:15 +0200

    Restore src with correct version+ working vapi

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+// Common types for the application
+export interface SetData {
+  exercise: string;
+  setNumber: number;
+  reps: number;
+  startTime: number;
+  endTime: number;
+  avgVelocity: number;
+  peakVelocity: number;
+  velocityLoss: number;
+  formScore: number;
+}
